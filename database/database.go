45def2e631a230a303bb4eeb1fc4bde530b415e7
jon4h <admin@psytux.com>
2021-03-31 01:50:20 +0200

    wip: database connection

--- database/database.go
+++ database/database.go
@@ -3,24 +3,34 @@ package database
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
 )
 
 type Config struct {
 	DBName     string `mapstructure:"POSTGRES_DB"`
-	DBUsername string `mapstructure:"POSTGRES_USER"`
+	DBUser     string `mapstructure:"POSTGRES_USER"`
 	DBPassword string `mapstructure:"POSTGRES_PASSWORD"`
 	DBServer   string `mapstructure:"POSTGRES_SERVER"`
 	DBPort     string `mapstructure:"POSTGRES_PORT"`
 }
 
-func InitDatabase(*Config) {
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+type testRows struct {
+	name string
+}
+
+func InitDatabase(dbpool *pgxpool.Pool) (err error) {
+
+	rows, err := dbpool.Query(context.Background(), "SELECT tablename FROM pg_catalog.pg_tables;")
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		log.Fatal("Unable to connect to database: ", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	defer rows.Close()
+	var test string
+	rows.Scan(&test)
+	fmt.Println(test)
+	return
 }
--- main.go
+++ main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"os"
 
+	"github.com/jackc/pgx/v4/pgxpool"
+
 	"github.com/jon4hz/go-binance-local-orderbook/config"
+	"github.com/jon4hz/go-binance-local-orderbook/database"
 	"github.com/jon4hz/go-binance-local-orderbook/exchange/binance"
 	"github.com/jon4hz/go-binance-local-orderbook/watchdog"
 )
 
 func main() {
+	// load configuration from file or env
 	config := loadConfiguration()
+
+	// get database pool
+	dbpool, err := createDatabasePool(config)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	err = database.InitDatabase(dbpool)
+
+	// start orderbook watchdog
 	go watchdog.Watcher()
+
+	// start the websocket to binance (blocking with channel)
 	binance.HandleWebsocket(config)
+}
+
+func createDatabasePool(config *config.Config) (dbpool *pgxpool.Pool, err error) {
+	// create database connection
+	pgxConfig, err := pgxpool.ParseConfig(fmt.Sprintf("postgres://%s:%s@%s:%s/%s", config.Database.DBUser, config.Database.DBPassword, config.Database.DBServer, config.Database.DBPort, config.Database.DBName))
+	if err != nil {
+		log.Fatal("Error configuring the database: ", err)
+	}
+	// create connection pool
+	dbpool, err = pgxpool.ConnectConfig(context.Background(), pgxConfig)
+	if err != nil {
+		log.Fatal("Unable to connect to database: ", err)
+		return
+	}
+	return
 }
 
 func loadConfiguration() *config.Config {
